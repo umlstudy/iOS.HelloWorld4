46d4f4072f56df657f581b708f8354a35773ecf0
sejong <umlstudy@gmail.com>
2014-10-29 00:38:21 +0900

    toolbar hide show

---
+++ HelloWorld4/MyImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyImageViewController.h
+//  HelloWorld4
+//
+//  Created by jomakson on 2014. 10. 28..
+//  Copyright (c) 2014년 jomakson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyImageViewController : UIViewController
+
+@end
---
+++ HelloWorld4/MyImageViewController.m
@@ -0,0 +1,37 @@
+//
+//  MyImageViewController.m
+//  HelloWorld4
+//
+//  Created by jomakson on 2014. 10. 28..
+//  Copyright (c) 2014년 jomakson. All rights reserved.
+//
+
+#import "MyImageViewController.h"
+
+@interface MyImageViewController ()
+
+@end
+
+@implementation MyImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
